c1bf6a13a37e37d5c9e22ead04fd7c1b3ae84fa7
Evgen <zhenya.bill@gmail.com>
2022-09-22 20:53:19 +0300

    start

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -3,23 +3,22 @@ import { Router } from "express";
 const router = Router();
 
 import { requestToDB } from "../elephantsql.js";
-import {filmRouter} from "./filmRouter.js";
+import { filmRouter } from "./filmRouter.js";
 
 router.get("/", async (req, res) => {
   const dbRequest = await requestToDB();
   res.send(dbRequest);
 });
 
-router.use('/film', filmRouter);
+router.use("/film", filmRouter);
 
 // @ts-ignore
-router.use('*', (err, req, res, next) => { // custom errorHandler
-  res
-      .status(err.status || 500)
-      .json({
-        message: err.message,
-        // data: err.data  // доп. поля в error
-      });
+router.use("*", (err, req, res, next) => {
+  // custom errorHandler
+  res.status(err.status || 500).json({
+    message: err.message,
+    // data: err.data  // доп. поля в error
+  });
 });
 
 export const apiRouter = router;
